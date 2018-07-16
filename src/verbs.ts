e01b1009b375d4e8dd8a5efb510fb4f203fdcea2
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-07-16 19:48:44 +0900

    add type Verb.

--- src/verbs.ts
+++ src/verbs.ts
@@ -23,11 +23,32 @@ import { getTextFromFirstArg } from "./util";
 
 async function main(): Promise<string> {
     const data = await getTextFromFirstArg();
-    const lines = data.split(/\r?\n/).map(line => line.split(/\t/))
-    const fields = lines.shift() as string[];
+    const lines = data.split(/\r?\n/).map(line => line.split(/\t/).map(x => x !== "" ? x : null))
+    const fields = lines.shift() as string[];    
     return fields.join("\n");
 }
+
 
 main()
     .then(console.log)
     .catch(console.error);
+
+interface Verb {
+    番号: string;
+    区分: string;
+    基本形: string;
+    意味: string;
+    語幹: string;
+    現在丁寧体: string;
+    現在会話体: string;
+    現在常体: string;
+    現在連体形: string;
+    連用形: string;
+    過去語幹: string;
+    過去丁寧体: string;
+    過去会話体: string;
+    過去常体: string;
+    過去連体形: string;
+    回想: string;
+    未来連体形: string;
+}

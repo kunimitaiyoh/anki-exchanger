ccbd2a98cdf4bebaa7b6a80e65ecb03c16eb6318
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-07-17 14:18:53 +0900

    add converter to verb.

--- src/verbs.ts
+++ src/verbs.ts
@@ -1,40 +1,28 @@
 import * as fs from "fs";
 import { getTextFromFirstArg } from "./util";
 
-// const fields = [
-//     "番号",
-//     "区分",
-//     "基本形",
-//     "意味",
-//     "語幹",
-//     "現在丁寧体",
-//     "現在会話体",
-//     "現在常体",
-//     "現在連体形",
-//     "連用形",
-//     "過去語幹",
-//     "過去丁寧体",
-//     "過去会話体",
-//     "過去常体",
-//     "過去連体形",
-//     "回想",
-//     "未来連体形",
-// ]
-
-async function main(): Promise<string> {
+async function main(): Promise<Verb[]> {
     const data = await getTextFromFirstArg();
     const lines = data.split(/\r?\n/).map(line => line.split(/\t/).map(x => x !== "" ? x : null))
-    const fields = lines.shift() as string[];    
-    return fields.join("\n");
+    const fieldDictionry = (lines.shift() as string[]).reduce((dictionary, x, i) => {
+        dictionary[i] = x;
+        return dictionary;
+    }, {} as { [key: number]: string })
+    return lines.map(x => {
+        return x.reduce((ys, y, i) => {
+            const key = fieldDictionry[i];
+            ys[key] = (key === "番号") ? parseInt(y as string) : y;
+            return ys;
+        }, {} as { [key: string]: string | number | null }) as any as Verb;
+    });
 }
-
 
 main()
     .then(console.log)
     .catch(console.error);
 
 interface Verb {
-    番号: string;
+    番号: number;
     区分: string;
     基本形: string;
     意味: string;
